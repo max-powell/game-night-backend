dc9318a906b8d6daec23e97799ad113735e74530
max-powell <github email address>
2019-05-15 16:14:08 +0100

    buitl user routes with nested games, event and friend routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :attendances
-  resources :events
-  resources :ownerships
-  resources :games
-  resources :friendships
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :create, :update] do
+    resources :games, only: :index
+    resources :events, only: :index
+    get '/friends', to: 'user#friends'
+  end
+  # resources :attendances
+  # resources :events
+  # resources :ownerships
+  # resources :friendships
 end
