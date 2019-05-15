724c7a22d83e5a966b5eb9e8560d7aa6a420536c
max-powell <github email address>
2019-05-15 17:30:47 +0100

    added owner_ids to game_params

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,7 +15,7 @@ class GamesController < ApplicationController
   private
 
   def game_params
-    params.require(:game).permit(:name, :min_players, :max_players, :min_playtime, :max_playtime, :description, :image_url)
+    params.require(:game).permit(:name, :min_players, :max_players, :min_playtime, :max_playtime, :description, :image_url, owner_ids: [])
   end
 
 end
