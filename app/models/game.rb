21a27ea59ba615e2380872a3a8ac624483dca645
max-powell <github email address>
2019-05-15 14:31:13 +0100

    added associations to models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,4 @@
 class Game < ApplicationRecord
+  has_many :ownerships, dependent: :destroy
+  has_many :owners, through: :ownerships, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
+  
+  has_many :ownerships, dependent: :destroy
+  has_many :games, through: :ownerships
 end
