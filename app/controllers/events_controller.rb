56a9cbb666e82c0d8ad42af987e5fddeb2705182
max-powell <github email address>
2019-05-15 17:45:34 +0100

    added strong params and index action to EventsController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,14 @@
 class EventsController < ApplicationController
+
+  def index
+    user = User.find(params[:user_id])
+    render json: user.events
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:game_id, :date_time, :location, :game_owner, attendee_ids: [])
+  end
+
 end
