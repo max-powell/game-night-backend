3c4adb0480a512453cf3b1ab858c3cffcb952472
max-powell <github email address>
2019-05-15 16:50:15 +0100

    added show, create & friends actions to UsersController. Fixed typo in /friends route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      render json: user
+    else
+      render json: {error: user.errors.full_messages}
+    end
+  end
+
+  def friends
+    byebug
+    user = User.find(params[:id])
+    render json: user.friends
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :avatar_url)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :create, :update] do
     resources :games, only: :index
     resources :events, only: :index
-    get '/friends', to: 'user#friends'
+    get '/friends', to: 'users#friends'
   end
 
 end
