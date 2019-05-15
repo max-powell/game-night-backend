b614ecac6ed42eb519ae2d48ea8fa30eafd729d7
max-powell <github email address>
2019-05-15 15:22:50 +0100

    Event validates game_owner present if game has been selected

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  validates :game_owner, presence: true, unless: Proc.new { |e| e.game_id.blank? }
+
   belongs_to :game, optional: true
 
   has_many :attendances, dependent: :destroy
