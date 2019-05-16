92fee1878f835fc00dc14a4913c6d68ca3974bfd
max-powell <github email address>
2019-05-16 15:03:58 +0100

    friend methods moved to own controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :update]
   resources :games, only: [:index, :create]
   resources :events, only: [:index, :create]
+  resources :friends, only: :index
 
   get '/login', to: 'auth#create'
   get '/profile', to: 'users#profile'
-  get '/friends', to: 'users#friends'
+  post '/friends', to: 'friends#add_friend'
 
 end
