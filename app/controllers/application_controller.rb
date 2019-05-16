f6a18718f9449a552d95631192ce605d6cf9b5f5
max-powell <github email address>
2019-05-16 10:29:31 +0100

    added encode and decode token to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def encode_token(payload)
+    JWT.encode(payload, 'secret')
+  end
+
+  def decoded_token(token)
+    JWT.decode(token, 'secret')[0]
+  end
 end
