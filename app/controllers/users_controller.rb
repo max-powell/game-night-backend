fe195342f3cfb29e0034b8246ad57f6648e212fc
max-powell <github email address>
2019-05-16 10:06:35 +0100

    added /login and /profile routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
   def create
     user = User.create(user_params)
     if user.valid?
-      render json: user
+      render json: user, status: :created
     else
-      render json: {error: user.errors.full_messages}
+      render json: {error: user.errors.full_messages}, status: :not_aceptable
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     resources :events, only: [:index, :create]
   end
 
+  get '/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
+
   resources :games, only: :update
 
   get 'users/:id/friends', to: 'users#friends'
