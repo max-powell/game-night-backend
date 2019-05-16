a13b7ab4867dd79bfa387c08939e899111c02268
max-powell <github email address>
2019-05-16 10:39:43 +0100

    added current_user, logged_in? and authorised methods to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,34 @@ class ApplicationController < ActionController::API
     JWT.encode(payload, 'secret')
   end
 
-  def decoded_token(token)
-    JWT.decode(token, 'secret')[0]
+  def auth_header
+    request.headers['Authorization']
   end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'secret', true, algorithm: 'HS256')[0]
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def current_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      user = User.find_by(id:user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorised
+    render json: {message: 'Please log in'}, status: :unauthorized unless logged_in?
+  end
+
 end
