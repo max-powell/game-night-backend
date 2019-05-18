feb4acd729585e6a12ae19975785c7d31fd66ade
max-powell <github email address>
2019-05-18 16:08:08 +0100

    added friend count to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :avatar_url, :game_count
+  attributes :id, :username, :avatar_url, :game_count, :friends
 
   def game_count
     object.games.count
+  end
+
+  def friend_count
+    object.friends.count
   end
 
 end
