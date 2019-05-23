417fa28701b41802422479b3c77f8d3eff7488d1
max-powell <github email address>
2019-05-23 17:19:33 +0100

    added game name to event summary serializer

--- app/serializers/invite_event_summary_serializer.rb
+++ app/serializers/invite_event_summary_serializer.rb
@@ -1,5 +1,9 @@
 class InviteEventSummarySerializer < ActiveModel::Serializer
-  attributes :id, :date_time, :location, :host
+  attributes :id, :date_time, :location, :host, :game
+
+  def game
+    object.game.name
+  end
 
   def host
     ActiveModelSerializers::SerializableResource.new(object.host, {serializer: InviteUserSerializer})
