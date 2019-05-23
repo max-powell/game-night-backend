aec8b6b321e06786e5cb85d5d5f5d4603f961a05
max-powell <github email address>
2019-05-23 15:19:34 +0100

    events are deleted if host deletes their attendance. added friend serializer to use in profile serializer. profile serializer uses associations

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :avatar_url, :game_count, :friend_count
+  attributes :id, :username, :avatar_url
+  has_many :games
+  has_many :friends, serializer: FriendSerializer
+  has_many :events, serializer: InviteEventSummarySerializer
 
-  def game_count
-    object.games.count
-  end
-
-  def friend_count
-    object.friends.count
-  end
 
 end
