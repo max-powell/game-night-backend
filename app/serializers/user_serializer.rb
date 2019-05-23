b332af7e828f2873251dc3389a17858b74506bf9
max-powell <github email address>
2019-05-23 16:00:48 +0100

    user serializer returns summary of friends, friend requests, games, events and event invites

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,19 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :avatar_url
+  attributes :id, :username, :avatar_url, :friend_requests, :event_invites
   has_many :games
   has_many :friends, serializer: FriendSerializer
   has_many :events, serializer: InviteEventSummarySerializer
 
+  def friend_requests
+    FriendRequest.where(friend: current_user).map do |r|
+      ActiveModelSerializers::SerializableResource.new(r, {serializer: FriendRequestSerializer})
+    end
+  end
+
+  def event_invites
+    object.event_invites.map do |i|
+      ActiveModelSerializers::SerializableResource.new(i, {serializer: EventInviteSerializer})
+    end
+  end
 
 end
