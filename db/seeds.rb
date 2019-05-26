bd8fd19990ace9352a43d401a1d527fad96bb2b7
max-powell <github email address>
2019-05-26 15:32:11 +0100

    loaded in games mechanics and set agg_scores

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def change_score(names, new_score)
+  names.each do |n|
+    m = Mechanic.find_by_name(n)
+    m.update(agg_score: new_score)
+  end
+end
+
+minus_one = 'Betting
+Comodity Speculation
+Deck Building
+Deduction
+Engine Building
+Euro Game
+Hand Management
+Memory
+Pattern Building
+Pattern Recognition
+Pick-up and Deliver
+Pool Building
+Press Your Luck
+Resource Gathering
+Route Building
+Route/Network Building
+Stock Holding
+Teams
+Partnerships
+Tile Placement
+Worker Placement'
+
+plus_one = 'Asymmetric
+Auction
+Bluffing
+Card Drafting
+Drafting
+Hidden Traitor
+Player Elimination
+Trick-taking'
+
+plus_two = 'Area Control
+Area Enclosure
+Fighting
+Player vs. Player
+Rock-Paper-Scissors
+Secret Unit Deployment
+Hidden Movement
+Take That
+Voting
+Wargame'
+
+# change_score(minus_one.split("\n"), -1)
+# change_score(plus_one.split("\n"), 1)
+# change_score(plus_two.split("\n"), 2)
