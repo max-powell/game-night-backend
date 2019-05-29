134495c920b37bd70590d7bb7dd8c60a5cbcc2b4
max-powell <github email address>
2019-05-29 10:40:08 +0100

    added destroy attendances route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :update]
   resources :games, only: [:index, :create]
   resources :events, only: [:index, :show, :create, :update]
+  resources :attendances, only: :destroy
   resources :friends, only: :index
   resources :friend_requests, except: [:show, :new, :edit]
   resources :event_invites, only: [:index, :create, :update, :destroy]
